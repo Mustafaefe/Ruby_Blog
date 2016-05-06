945e932c40bd5359788c3b3c187a7c334892ec0d
Mustafaefe <m.efe_1905@hotmail.com>
2016-05-06 10:58:48 +0300

    girilen postların silineblimesi için index sayfasına link oluşturuldu, controller sayfası düzenlendi

--- Blog/app/controllers/posts_controller.rb
+++ Blog/app/controllers/posts_controller.rb
@@ -34,7 +34,11 @@ class PostsController < ApplicationController
   		end
 	end
 
-
+	def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+  	    redirect_to @post
+	end
 	
 	private
         def post_params
