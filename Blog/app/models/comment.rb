5d9a1ae62870299f206a5f7ce364ecd0b22e5a75
Mustafaefe <m.efe_1905@hotmail.com>
2016-05-06 10:27:25 +0300

    posts/index sayfası için göster ve güncelle linkleri eklendi

--- Blog/app/models/comment.rb
+++ Blog/app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :post
+	validates :email, presence: true
+		,format: { with: /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/ }
 end
--- Blog/app/models/post.rb
+++ Blog/app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
 	has_many :comment
+	validates :title, presence: true,
+                    length: { minimum: 5 }
+	validates :text, presence:true	
 end
