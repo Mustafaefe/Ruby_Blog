66e54344ee0bd87ef82cf24b7fda351a3c0f6280
Mustafaefe <m.efe_1905@hotmail.com>
2016-05-06 10:01:01 +0300

    Welcome sayfasına post sayfası için linkler koyuldu

--- Blog/db/migrate/20160505190558_create_posts.rb
+++ Blog/db/migrate/20160505190558_create_posts.rb
@@ -6,5 +6,15 @@ class CreatePosts < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    create_table :comments do |t|
+      t.belongs_to :post, index:true
+      t.integer :post_id
+      t.string :email
+      t.text :comment
+
+      t.timestamps null: false
+    end
+
   end
 end
